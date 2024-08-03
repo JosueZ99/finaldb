1c3ada6dd097f3a97caf5139433e6218bdbc6690
Josue Zambrano <zambrano.josue.99@gmail.com>
2024-08-03 02:01:43 -0500

    consultas clientes e inventario

--- videojuegos/urls.py
+++ videojuegos/urls.py
@@ -8,9 +8,9 @@ urlpatterns = [
     path("clientes/", views.clientes, name="clientes"),
     path('clientes/registro/', views.ingresar_cliente, name='registro'),
     path('clientes/actualizar/<str:cedula>/', views.actualizar_cliente, name='actualizar_cliente'),
-    path('clientes/consultar/<str:cedula>/', views.consultar_cliente, name='consultar_cliente'),
+    path('clientes/consultar/', views.consultar_cliente, name='consultar_cliente'),
     path("inventario/", views.inventario, name="inventario"),
     path('inventario/ingresar/', views.ingresar_inventario, name='ingresar'),
-    path('inventario/actualizar/', views.actualizar_inventario, name='actualizacion_inventario'),
     path('inventario/actualizar/<str:codigo_producto>/', views.actualizar_inventario, name='actualizar_inventario'),
+    path('inventario/consultar/', views.consultar_inventario, name='consultar_inventario'),
 ]
--- videojuegos/views.py
+++ videojuegos/views.py
@@ -78,37 +78,37 @@ def actualizar_cliente(request, cedula):
         
     return render(request, 'actualizar_cliente.html', {'form': form})
 
-def consultar_cliente(request, cedula):
+def consultar_cliente(request):
+    cedula = request.GET.get('cedula', '')
     cliente_info = None
     mensaje = 'No se encontró ningún cliente'
     try:
-        with connection.cursor() as cursor:
-            cursor.execute("CALL consultar_cliente(%s)", [cedula])
-            conn = cursor.connection
-            conn.poll()  # Actualiza la conexión para capturar los mensajes de notificación
-            notices = conn.notices
-            
-            cliente_info = {}
-            for notice in notices:
-                # Verifica y procesa los mensajes de notificación
-                match = re.match(
-                    r"ID: (\d+), Cédula: (\S+), Apellidos y Nombres: (.+?), Correo: (.+?), Provincia: (.+?), Dirección: (.+?), Estado Cliente: (.+?), Teléfono: (.+?)",
-                    notice
-                )
-                if match:
-                    cliente_info = {
-                        'id': match.group(1),
-                        'cedula': match.group(2),
-                        'apellidos_nombres': match.group(3),
-                        'correo': match.group(4),
-                        'provincia': match.group(5),
-                        'direccion': match.group(6),
-                        'estado': match.group(7),
-                        'telefono': match.group(8),
-                    }
-                else:
-                    mensaje = notice
-                    
+        if cedula:
+            with connection.cursor() as cursor:
+                cursor.execute("CALL consultar_cliente(%s)", [cedula])
+                conn = cursor.connection
+                conn.poll()
+                notices = conn.notices
+                
+                cliente_info = {}
+                for notice in notices:
+                    match = re.match(
+                        r"NOTICE:  ID: (\d+), Cédula: (\S+), Apellidos y Nombres: (.+?), Correo: (.+?), Provincia: (.+?), Dirección: (.+?), Estado Cliente: (.+?), Teléfono: ([\d\s\+\-]+)",
+                        notice
+                    )
+                    if match:
+                        cliente_info = {
+                            'id': match.group(1),
+                            'cedula': match.group(2),
+                            'apellidos_nombres': match.group(3),
+                            'correo': match.group(4),
+                            'provincia': match.group(5),
+                            'direccion': match.group(6),
+                            'estado': match.group(7),
+                            'telefono': match.group(8),
+                        }
+                    else:
+                        mensaje = notice
     except Exception as e:
         mensaje = str(e)
         
@@ -185,3 +185,46 @@ def actualizar_inventario(request, codigo_producto):
         })
     
     return render(request, 'actualizar_inventario.html', {'form': form})
+
+def consultar_inventario(request):
+    codigo_producto = request.GET.get('codigo_producto', '')
+    inventario_info = None
+    mensaje = 'No se encontró ningún producto en el inventario'
+    try:
+        if codigo_producto:
+            with connection.cursor() as cursor:
+                cursor.execute("CALL consultar_inventario(%s)", [codigo_producto])
+                conn = cursor.connection
+                conn.poll()
+                notices = conn.notices
+                
+                inventario_info = {}
+                for notice in notices:
+                    match = re.match(
+                        r"NOTICE:  ID: (\d+), Código de Producto: (\S+), Nombre: (.+?), Formato: (.+?), Género: (.+?), Plataforma: (.+?), Año de Lanzamiento: (\d+), Precio: (\d+\.\d+), Stock: (\d+), Estado de Producto: (.+)",
+                        notice
+                    )
+                    if match:
+                        inventario_info = {
+                            'id': match.group(1),
+                            'codigo_producto': match.group(2),
+                            'nombre': match.group(3),
+                            'formato': match.group(4),
+                            'genero': match.group(5),
+                            'plataforma': match.group(6),
+                            'ano_lanzamiento': match.group(7),
+                            'precio': match.group(8),
+                            'stock': match.group(9),
+                            'estado': match.group(10),
+                        }
+                    else:
+                        mensaje = notice
+    except Exception as e:
+        mensaje = str(e)
+        
+    context = {
+        'inventario_info': inventario_info,
+        'mensaje': mensaje,
+    }
+
+    return render(request, 'consultar_inventario.html', context)
