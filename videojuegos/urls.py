75af86616d363d85c58a6253b2b1ef7a833350bc
Josue Zambrano <zambrano.josue.99@gmail.com>
2024-08-02 22:35:50 -0500

    formularios ingresar y actualizar cliente

--- videojuegos/urls.py
+++ videojuegos/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("clientes/", views.clientes, name="clientes"),
     path('clientes/registro/', views.ingresar_cliente, name='registro'),
-    path('clientes/actualizacion/', views.actualizar_cliente, name='actualizacion'),
+    path('clientes/actualizar/', views.actualizar_cliente, name='actualizacion'),
+    path('clientes/actualizar/<str:cedula>/', views.actualizar_cliente, name='actualizar_cliente'),
 ]
